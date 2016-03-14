861cadac48d191c87ac7d6348da33af7b6fc23a1
fzykzc <jeff.wise@gm.com>
2016-03-14 09:55:20 -0400

    initial commit

---
+++ DelimitedTextHelper/DelimitedTextParser.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DelimitedTextHelper
+{
+    public class DelimitedTextParser : IDisposable
+    {
+        private TextReader _reader;
+        private char _delimiter;
+        private static Regex rexCsvSplitter = new Regex(@",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))");
+        private static Regex rexRunOnLine = new Regex(@"^[^""]*(?:""[^""]*""[^""]*)*""[^""]*$");
+
+        private const string QUOTE = "\"";
+        private const string ESCAPED_QUOTE = "\"\"";
+        private char[] CHARACTERS_THAT_MUST_BE_QUOTED = { ',', '"', '\n' };
+
+        public DelimitedTextParser(TextReader reader):this(reader, ',')
+        {
+
+        }
+
+        public DelimitedTextParser(TextReader reader, char delimiter)
+        {
+            _delimiter = delimiter;
+            _reader = reader;
+        }
+
+        public virtual string[] Read()
+        {
+            try
+            {
+                var row = ReadLine();
+                
+                return row;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        protected virtual string[] ReadLine()
+        {
+            try
+            {
+                string[] record = null;
+                if (_reader != null)
+                {
+                    var row = _reader.ReadLine();
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        record = GetRow(row);
+                    }
+                }
+
+                return record;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IEnumerable<string> SplitRow(string row, char delimiter = ',')
+        {
+            var currentString = new StringBuilder();
+            var inQuotes = false;
+            var quoteIsEscaped = false; //Store when a quote has been escaped.
+            row = string.Format("{0}{1}", row, delimiter); //We add new cells at the delimiter, so append one for the parser.
+            foreach (var character in row.ToCharArray().Select((val, index) => new { val, index }))
+            {
+                if (character.val == delimiter) //We hit a delimiter character...
+                {
+                    if (!inQuotes) //Are we inside quotes? If not, we've hit the end of a cell value.
+                    {
+                        //Console.WriteLine(currentString);
+                        yield return currentString.ToString();
+                        currentString.Clear();
+                    }
+                    else
+                    {
+                        currentString.Append(character.val);
+                    }
+                }
+                else
+                {
+                    if (character.val != ' ')
+                    {
+                        if (character.val == '"') //If we've hit a quote character...
+                        {
+                            if (character.val == '"' && inQuotes) //Does it appear to be a closing quote?
+                            {
+                                if (row[character.index + 1] == character.val && !quoteIsEscaped) //If the character afterwards is also a quote, this is to escape that (not a closing quote).
+                                {
+                                    quoteIsEscaped = true; //Flag that we are escaped for the next character. Don't add the escaping quote.
+                                }
+                                else if (quoteIsEscaped)
+                                {
+                                    quoteIsEscaped = false; //This is an escaped quote. Add it and revert quoteIsEscaped to false.
+                                    currentString.Append(character.val);
+                                }
+                                else
+                                {
+                                    inQuotes = false;
+                                }
+                            }
+                            else
+                            {
+                                if (!inQuotes)
+                                {
+                                    inQuotes = true;
+                                }
+                                else
+                                {
+                                    currentString.Append(character.val); //...It's a quote inside a quote.
+                                }
+                            }
+                        }
+                        else
+                        {
+                            currentString.Append(character.val);
+                        }
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrWhiteSpace(currentString.ToString())) //Append only if not new cell
+                        {
+                            currentString.Append(character.val);
+                        }
+                    }
+                }
+            }
+        }
+
+        //public string[] GetRow(string sLine)
+        //{
+
+
+        //        string[] values = rexCsvSplitter.Split(sLine);
+
+        //    for (int i = 0; i < values.Length; i++)
+        //    {
+        //        values[i] = Unescape(values[i]);
+        //    }
+
+        //    return values;
+        //}
+
+        public string[] GetRow(string csvText)
+        {
+            List<string> tokens = new List<string>();
+
+            int last = -1;
+            int current = 0;
+            bool inText = false;
+
+            while (current < csvText.Length)
+            {
+                switch (csvText[current])
+                {
+                    case '"':
+                        inText = !inText; break;
+                    case ',':
+                        if (!inText)
+                        {
+                            tokens.Add(Unescape(csvText.Substring(last + 1, (current - last)).Trim(',')));
+                            last = current;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                current++;
+            }
+
+            if (last != csvText.Length - 1)
+            {
+                tokens.Add(Unescape(csvText.Substring(last + 1)));
+            }
+
+            return tokens.ToArray();
+        }
+
+        public string Escape(string s)
+        {
+            if (s.Contains(QUOTE))
+                s = s.Replace(QUOTE, ESCAPED_QUOTE);
+
+            if (s.IndexOfAny(CHARACTERS_THAT_MUST_BE_QUOTED) > -1)
+                s = QUOTE + s + QUOTE;
+
+            return s;
+        }
+
+        public string Unescape(string s)
+        {
+            if (s.StartsWith(QUOTE) && s.EndsWith(QUOTE))
+            {
+                s = s.Substring(1, s.Length - 2);
+
+                if (s.Contains(ESCAPED_QUOTE))
+                    s = s.Replace(ESCAPED_QUOTE, QUOTE);
+            }
+
+            return s;
+        }
+
+        
+
+        public void Dispose()
+        {
+            if(_reader != null)
+            {
+                _reader = null;
+            }
+        }
+    }
+
+}
---
+++ DelimitedTextHelper/DelimitedTextReader.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelimitedTextHelper
+{
+    public class DelimitedTextReader : IDisposable
+    {
+        private DelimitedTextParser _parser;
+        private string[] _currentRecord;
+        private string[] _headerRecord;
+        private int _currentIndex = -1;
+        private bool _doneReading;
+        private bool _hasBeenRead;
+        
+        public virtual bool AreFieldHeadersCaseSensitive { get; set; }
+        public virtual bool FirstRowIsHeader { get; set; }
+        /// <summary>
+		/// Gets the field headers.
+		/// </summary>
+		public virtual string[] FieldHeaders
+        {
+            get
+            {
+                throwIfParserHasNotBeenRead();
+                return _headerRecord;
+            }
+        }
+
+        /// <summary>
+        /// Get the current record;
+        /// </summary>
+        public virtual string[] CurrentRecord
+        {
+            get
+            {
+                throwIfParserHasNotBeenRead();
+                return _currentRecord;
+            }
+        }
+        
+        public DelimitedTextReader(TextReader reader):this(reader, ',')
+        {
+        }
+
+        public DelimitedTextReader(TextReader reader, char delimiter)
+        {
+            _parser = new DelimitedTextParser(reader, delimiter);
+        }
+                
+        public virtual bool Read()
+        {
+            //CheckDisposed();
+
+            if (_doneReading)
+            {
+                return false;
+            }
+
+            if (FirstRowIsHeader && _headerRecord == null)
+            {
+                do
+                {
+                    _currentRecord = _parser.Read();
+                }
+                while (SkipRecord());
+                _headerRecord = _currentRecord;
+                _currentRecord = null;
+                ParseNamedIndexes();
+            }
+
+            do
+            {
+                _currentRecord = _parser.Read();
+            }
+            while (SkipRecord());
+
+            _currentIndex = -1;
+            _hasBeenRead = true;
+
+            if (_currentRecord == null)
+            {
+                _doneReading = true;
+            }
+
+            return _currentRecord != null;
+        }
+
+        public TEntity GetRecord<TEntity>() where TEntity : new()
+        {
+            throwIfParserHasNotBeenRead();
+            TEntity record;
+            try
+            {
+                record = createRecord<TEntity>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return record;
+        }
+
+        public bool SkipRecord()
+        {
+            return false;
+        }
+
+        public void ParseNamedIndexes()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        private TEntity createRecord<TEntity>() where TEntity:new()
+        {
+            TEntity record;
+            try
+            {
+                record = new TEntity();
+                if (FirstRowIsHeader)
+                {
+                    //try to auto map the header names to properties on the class
+                    //first get all of the property infos for the type:
+                    buildPropertyMappings<TEntity>();
+                    for(int i = 0; i < _headerRecord.Length;i++)
+                    {
+                        var item = _headerRecord[i];                        
+                        if(_propertyMappings.ContainsKey(item))
+                        {
+                            var value = Convert.ChangeType(CurrentRecord[i], _propertyMappings[item].PropertyType);                           
+                            _propertyMappings[item].SetValue(record, value);
+                        }
+                    }
+                }
+                return record;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private Dictionary<string, PropertyInfo> _propertyMappings = new Dictionary<string, PropertyInfo>();
+        private void buildPropertyMappings<TRecord>()
+        {
+            if (FirstRowIsHeader)
+            {
+                //try to auto map the header names to properties on the class
+                //first get all of the property infos for the type:
+                var propertyCount = typeof(TRecord).GetRuntimeProperties().Count();
+                
+                for (int i = 0; i < _headerRecord.Length; i++)
+                {
+                    var item =  _headerRecord[i];
+                    var prop = GetProperty<TRecord>(item);
+                    if (prop != null)
+                    {
+                        _propertyMappings.Add(_headerRecord[i], prop);
+                    }
+                }                
+            }
+            else
+            {
+                //build property map by index.  map according to the property name in order it appears
+                var properties = typeof(TRecord).GetRuntimeProperties();
+            }
+        }
+
+        private PropertyInfo GetProperty<T>( string propertyName)
+        {
+            BindingFlags flags = BindingFlags.Public | BindingFlags.Instance;
+            if (!AreFieldHeadersCaseSensitive)
+            {
+                flags = flags | BindingFlags.IgnoreCase;
+            }
+            PropertyInfo pi = typeof(T).GetProperty(propertyName, flags);
+            return pi;
+        }
+
+        private void throwIfParserHasNotBeenRead()
+        {
+            if (!_hasBeenRead)
+            {
+                throw new Exception("Read must be invoked before data can bee accessed.");
+            }
+        }
+    }
+}
